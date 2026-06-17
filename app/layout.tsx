19e61761f9e57137e11e94b31acb6dddfa53691b
zaki <jakiathallah@gmail.com>
2026-06-17 08:44:07 +0700

    feat: portfolio windows xp is ready to deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import "../styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Zaki's Portfolio — Windows XP Edition",
